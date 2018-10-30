d699022937adc9dbfee1876635754f01c1f8f373
1hemant <demonvsgod@gmail.com>
2018-10-30 14:39:12 +0530

    Added detail, message components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { UserDetailComponent }  from './user-detail/user-detail.component';
+import { UsersComponent }      from './users/users.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     UsersComponent,
     UserDetailComponent,
     MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
   ],
   bootstrap: [ AppComponent ]
 })
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,9 +6,7 @@ import { User } from './user';
 import { USERS } from './mock-users';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class UserService {
 
   constructor(private messageService: MessageService) { }
@@ -17,5 +15,11 @@ export class UserService {
     // TODO: send the message _after_ fetching the users
     this.messageService.add('UserService: fetched users');
     return of(USERS);
+  }
+
+  getUser(id: number): Observable<User> {
+    // TODO: send the message _after_ fetching the user
+    this.messageService.add(`UserService: fetched user id=${id}`);
+    return of(USERS.find(user => user.id === id));
   }
 }
