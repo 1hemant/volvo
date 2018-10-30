ce135de3174d9b5f37e0d525aab529f9ff4fb772
1hemant <demonvsgod@gmail.com>
2018-10-30 13:15:18 +0530

    User component works

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../user';
+ 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  user: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+ 
   constructor() { }
-
+ 
   ngOnInit() {
   }
-
+ 
 }
