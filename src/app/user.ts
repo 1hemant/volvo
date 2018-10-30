fc00c8129e65069c3ee993b94a436f8085bf6e86
1hemant <demonvsgod@gmail.com>
2018-10-30 21:28:48 +0530

    new table

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,10 @@
 export class User {
     id: number;
     name: string;
+    surname: string;
+    birthDate: Date;
+    phone: number;
+    city: string;
+    street: string;
+    number: number;
   }
-  
