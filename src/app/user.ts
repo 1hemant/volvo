25ded879a61d2fc6520a1a035ec01e537b69b791
1hemant <demonvsgod@gmail.com>
2018-10-30 13:47:43 +0530

    Displays list

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,5 @@
-export class Hero {
+export class User {
     id: number;
     name: string;
   }
+  
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../user';
- 
+import { User } from '../user';
+import { USERS } from '../mock-users';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent implements OnInit {
-  user: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
- 
+
+  users = USERS;
+  selectedUser: User;
+
   constructor() { }
- 
+
   ngOnInit() {
   }
- 
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
 }
